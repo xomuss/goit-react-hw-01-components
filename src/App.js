3a4aa7b05089a723b32de34338070bf779ff8750
Alexander Khomenko <72103285+xomuss@users.noreply.github.com>
2021-06-04 23:35:09 +0300

    task2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import Profile from './components/profile/Profile';
 import { name, tag, location, avatar, stats } from './user.json';
+import Statistics from './components/statistics/Statistics';
+import statisticalData from './statistical-data.json';
 
 const App = () => {
   return (
-    <Profile
-      name={name}
-      tag={tag}
-      location={location}
-      avatar={avatar}
-      followers={stats.followers}
-      views={stats.views}
-      likes={stats.likes}
-    />
+    <>
+      <Profile
+        name={name}
+        tag={tag}
+        location={location}
+        avatar={avatar}
+        followers={stats.followers}
+        views={stats.views}
+        likes={stats.likes}
+      />
+      <Statistics title="Upload stats" stats={statisticalData} />
+    </>
   );
 };
 
--- src/components/statistics/Statistics.js
+++ src/components/statistics/Statistics.js
@@ -1,5 +1,34 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Statistics = () => {
-  return <div></div>;
+const Statistics = ({ title, stats }) => (
+  <section class="statistics">
+    {title && <h2 class="title">{title}</h2>}
+
+    <ul class="stat-list">
+      {stats.map(({ id, label, percentage }) => (
+        <li class="item" key={id}>
+          <span class="label">{label}</span>
+          <span class="percentage">{percentage}%</span>
+        </li>
+      ))}
+    </ul>
+  </section>
+);
+
+Statistics.defaultProps = {
+  title: '',
 };
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    }),
+  ).isRequired,
+};
+
+export default Statistics;
